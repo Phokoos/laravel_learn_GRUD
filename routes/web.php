94c3013fef74e67794aec14e7e6c662045926a63
Phokoos <83214849+Phokoos@users.noreply.github.com>
2023-12-07 21:21:02 +0200

    Finished 85

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Address;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 //    return view('welcome');
     echo "Hello";
+});
+
+Route::get('/insert', function (){
+   $user = User::findOrFail(1);
+
+   $address = new Address(["name"=>"street_test_two"]);
+
+   $user->address()->save($address);
+});
+
+Route::get('/update', function (){
+    $address = Address::whereUserId( 1)->first();
+
+    $address->name = 'new';
+
+    $address->save();
+
+    return $address;
 });
