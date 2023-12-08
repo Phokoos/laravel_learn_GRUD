479b98f382e5792b906abbaf7b001710eae2e35a
Phokoos <83214849+Phokoos@users.noreply.github.com>
2023-12-08 17:10:42 +0200

    Finished 87

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| ONE TO ONE RELATIONS SHIP GRUD
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
@@ -22,18 +17,30 @@ Route::get('/', function () {
 
 Route::get('/insert', function (){
    $user = User::findOrFail(1);
-
    $address = new Address(["name"=>"street_test_two"]);
-
    $user->address()->save($address);
 });
 
 Route::get('/update', function (){
     $address = Address::whereUserId( 1)->first();
-
     $address->name = 'new';
-
     $address->save();
-
     return $address;
 });
+
+Route::get('/read', function (){
+    $user = User::findOrFail(1);
+    echo $user->address->name;
+});
+
+Route::get('/delete', function (){
+    $user = User::findOrFail(1);
+    $user->address()->delete();
+    echo $user;
+});
+
+/*
+|--------------------------------------------------------------------------
+| !!!
+|--------------------------------------------------------------------------
+*/
